8973ebcab3976b553d0430cc7c637d7a9efe91be
pkeane <pkeane92@yahoo.com>
2019-04-02 19:35:44 -0500

    made change to server.js so that hopefully index.html will display successfully on Heroku homepage

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
-    res.json(path.join(__dirname, "public/index.html"));
+    res.json(path.join(__dirname, "/index.html"));
 })
 
 app.listen(PORT, function(){
